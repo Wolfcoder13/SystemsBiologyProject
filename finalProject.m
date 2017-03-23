207687b65323553acc3dfabade7347757702eaea
Wolfcoder13 <Wolfninja13@gmail.com>
2017-03-23 20:55:22 +0000

    first commit to put everything on github

---
+++ finalProject.m
@@ -0,0 +1,7 @@
+% here we will do our matlab code for the final project... perhaps we
+% should use github for this one.
+
+%Start by simply adding reactions to our yeast7 model to create vanillin
+
+
+%get gurobi to work with matlab.
